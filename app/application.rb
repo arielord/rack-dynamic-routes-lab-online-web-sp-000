53f9ae7a0363f8195294f5b43d5e3d7e2b68b62a
Ariel <arielord@buffalo.edu>
2019-03-22 03:10:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -9,7 +9,7 @@ class Application
       item_name = req.path.split("/items/").last
       item = @@items.find {|i| i.name == item_name}
       
-      if item.class.to_s == 
+      if item.class.to_s == "Item"
         resp.write "#{item.price}"
         resp.status = 200
       else
