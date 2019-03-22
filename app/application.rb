7e4cd1f820bdce4ce629fd088651237da5ed9e27
Ariel <arielord@buffalo.edu>
2019-03-22 02:38:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,14 @@
 class Application
   def call(env)
     resp = Rack::Response.new()
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      
+    else
+      resp.status
+      
+    end
     
     resp.finish
   end
